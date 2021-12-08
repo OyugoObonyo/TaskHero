2d08ec8a03a44cd0ec1ca3e1e5a6c70f437b1ddd
OyugoObonyo <nyikal1999@gmail.com>
2021-12-08 15:08:10 +0300

    Add custom email and username validators

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, BooleanField, StringField
-from wtforms.fields.simple import SubmitField
-from wtforms.validators import DataRequired, Email
-
+from wtforms import PasswordField, BooleanField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, ValidationError, EqualTo
+from app.models import User
 
 class LoginForm(FlaskForm):
     """
@@ -19,7 +18,24 @@ class RegistrationForm(FlaskForm):
     Form to be filled in by new users
     """
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email address', validators=[DataRequired()])
+    email = StringField('Email address', validators=[DataRequired(), Email()])
     password1 = PasswordField('Password', validators=[DataRequired()])
-    password2 = PasswordField('Repeat password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat password', validators=[DataRequired(), EqualTo('password1')])
     submit = SubmitField('Sign up')
+
+    def validate_username(self):
+        """
+        Method checks whether a username already exists in the database
+        """
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different username.')
+
+    def validate_email(self):
+        """
+        Method checks whether a similar email has already been used to register a different user
+        """
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('An account with a similar email address already exists.')
+
