0cc3c84e07254ad23e4a88678aea97a64997461a
OyugoObonyo <nyikal1999@gmail.com>
2021-12-08 15:42:38 +0300

    Include database import in file

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app, db
 from flask import render_template, redirect, flash, url_for
 from app.forms import LoginForm, RegistrationForm
 from app.models import User
@@ -24,13 +24,13 @@ def login():
     return render_template('/login.html', title = 'Sign In', form = form)
 
 @app.route('/register', methods=['GET', 'POST'])
-def register_user():
+def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
+        user.set_password(form.password1.data)
         db.session.add(user)
         db.session.commit()
         flash('Sign up was successful!')
@@ -40,4 +40,4 @@ def register_user():
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
