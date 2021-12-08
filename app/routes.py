b344b7ba0c1a08994e53448ac17dce732a14e408
OyugoObonyo <nyikal1999@gmail.com>
2021-12-08 15:09:57 +0300

    Add view for user registration

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from app import app
 from flask import render_template, redirect, flash, url_for
 from app.forms import LoginForm, RegistrationForm
-
+from app.models import User
+from flask_login import current_user, login_user, logout_user
 
 @app.route('/')
 @app.route('/index')
@@ -10,12 +11,33 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
         return redirect(url_for('index'))
     return render_template('/login.html', title = 'Sign In', form = form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_user():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = RegistrationForm()
-    return render_template('/register.html', title = 'Sign Up', form = form)
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Sign up was successful!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
